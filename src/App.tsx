9a4538608c9df1ca4e410c546a54b33aa5c268cb
Andre Santos <andre@dexdev.com.br>
2020-06-08 01:03:27 -0300

    refactor: refactor index and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { StylesProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './styles/theme';
+import GlobalStyle from '~/styles/global';
+import { AuthContext } from './hooks/AuthContext';
+import { initialState, reducer } from './store/reducer';
+
+import Routes from '~/routes';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <h1> Hello </h1>
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <CssBaseline />
+          <Routes />
+          <GlobalStyle />
+        </StylesProvider>
+      </ThemeProvider>
+    </AuthContext.Provider>
   );
 }
 
